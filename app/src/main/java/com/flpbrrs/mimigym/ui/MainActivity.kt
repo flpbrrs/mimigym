7cccc438a07965a025170eee86b76c9b4c36c83e
Felipe Jonathan <felipejonathan.dev@gmail.com>
2025-08-22 12:15:26 -0300

    refactor: Move MainActivity to ui package and add dimens
    - Move `MainActivity.kt` from `com.flpbrrs.mimigym` to `com.flpbrrs.mimigym.ui`
    - Update `AndroidManifest.xml` to reflect the new location of `MainActivity`
    - Update `tools:context` in `activity_main.xml` to point to the new location of `MainActivity`
    - Add new dimension resources in `dimens.xml`

--- app/src/main/java/com/flpbrrs/mimigym/ui/MainActivity.kt
+++ app/src/main/java/com/flpbrrs/mimigym/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.flpbrrs.mimigym
+package com.flpbrrs.mimigym.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.flpbrrs.mimigym.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
