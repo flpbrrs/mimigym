187f1585a8c97b3ce9b9536997e09f2c3fee3c59
Felipe Jonathan <felipejonathan.dev@gmail.com>
2026-04-22 22:49:45 -0300

    refactor (exercise): update standard values for getExercisesList

--- app/src/main/java/com/flpbrrs/mimigym/features/exercises/domain/repository/ExercisesRepository.kt
+++ app/src/main/java/com/flpbrrs/mimigym/features/exercises/domain/repository/ExercisesRepository.kt
@@ -5,7 +5,11 @@ import com.flpbrrs.mimigym.features.exercises.domain.model.MuscleGroup
 import kotlinx.coroutines.flow.Flow
 
 interface ExercisesRepository {
-    fun getExerciseList(name: String?, muscleGroups: MuscleGroup?): Flow<List<Exercise>>
+    fun getExerciseList(
+        name: String? = null,
+        muscleGroups: MuscleGroup? = null
+    ): Flow<List<Exercise>>
+
     fun getUsedMuscleGroups(): Flow<List<MuscleGroup>>
     suspend fun getExerciseByNameAndMuscleGroup(name: String, muscleGroup: MuscleGroup): Exercise?
     suspend fun getTemplateCountByExercise(id: Long): Int
