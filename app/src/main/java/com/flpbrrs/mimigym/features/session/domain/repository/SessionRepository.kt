9f73d49201f4dbb04eb4acbdd6c4b4735997e6a5
Felipe Jonathan <felipejonathan.dev@gmail.com>
2026-07-04 18:27:56 -0300

    feat: definindo método inicial do SessionRepository.kt

--- app/src/main/java/com/flpbrrs/mimigym/features/session/domain/repository/SessionRepository.kt
+++ app/src/main/java/com/flpbrrs/mimigym/features/session/domain/repository/SessionRepository.kt
@@ -1,5 +1,11 @@
 package com.flpbrrs.mimigym.features.session.domain.repository
 
+import com.flpbrrs.mimigym.features.session.domain.model.WorkoutSession
+import java.time.LocalDate
+
 interface SessionRepository {
-    // TODO: Implementar o contrato para a feature de calendário da home
+    fun getSessionsBetween(
+        startDate: LocalDate,
+        endDate: LocalDate,
+    ): List<WorkoutSession>
 }
