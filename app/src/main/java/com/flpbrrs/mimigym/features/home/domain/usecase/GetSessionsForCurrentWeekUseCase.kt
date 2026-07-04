0ed449623b36a6eaad95a83c95dc9685bb539e09
Felipe Jonathan <felipejonathan.dev@gmail.com>
2026-07-04 18:28:30 -0300

    feat: definindo estrutura do caso de uso GetSessionsForCurrentWeekUseCase.kt

--- app/src/main/java/com/flpbrrs/mimigym/features/home/domain/usecase/GetSessionsForCurrentWeekUseCase.kt
+++ app/src/main/java/com/flpbrrs/mimigym/features/home/domain/usecase/GetSessionsForCurrentWeekUseCase.kt
@@ -1,3 +1,22 @@
 package com.flpbrrs.mimigym.features.home.domain.usecase
 
-class GetSessionsForCurrentWeekUseCase
+import com.flpbrrs.mimigym.features.home.domain.model.DayStatus
+import com.flpbrrs.mimigym.features.session.domain.repository.SessionRepository
+import com.flpbrrs.mimigym.features.template.domain.model.DayOfWeek
+import com.flpbrrs.mimigym.features.template.domain.repository.ScheduleRepository
+import java.time.LocalDate
+
+data class GetSessionsForCurrentWeekResponse(
+    val dayOfWeek: DayOfWeek,
+    val templateId: Long?,
+    val status: DayStatus,
+)
+
+class GetSessionsForCurrentWeekUseCase(
+    private val sessionRepository: SessionRepository,
+    private val scheduleRepository: ScheduleRepository,
+) {
+    operator fun invoke(referenceDate: LocalDate): List<GetSessionsForCurrentWeekResponse> {
+        TODO()
+    }
+}
