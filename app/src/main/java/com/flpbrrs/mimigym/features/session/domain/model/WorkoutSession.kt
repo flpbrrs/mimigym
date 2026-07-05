d0a3bf82030a6f32917b532b4dbd67b1619e7a59
Felipe Jonathan <felipejonathan.dev@gmail.com>
2026-07-04 21:46:49 -0300

    refactor: ajustando tipo de data em WorkoutSession.kt

--- app/src/main/java/com/flpbrrs/mimigym/features/session/domain/model/WorkoutSession.kt
+++ app/src/main/java/com/flpbrrs/mimigym/features/session/domain/model/WorkoutSession.kt
@@ -1,11 +1,11 @@
 package com.flpbrrs.mimigym.features.session.domain.model
 
-import java.time.LocalDate
+import java.time.LocalDateTime
 
 data class WorkoutSession(
     val id: Long,
     val templateId: Long,
-    val startedAt: LocalDate,
-    val finishedAt: LocalDate?,
+    val startedAt: LocalDateTime,
+    val finishedAt: LocalDateTime?,
     val note: String?,
 )
